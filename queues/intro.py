38d7b70d8f41308b0afa9a15bf99acfea0c4d702
Andrew Berns <andrew.berns@uni.edu>
2026-09-18 09:33:19 -0500

    Lecture 4.3

--- queues/intro.py
+++ queues/intro.py
@@ -8,3 +8,39 @@
 
 # On Friday, we'll do an activity much like the Lab 4 activity, only with queues
 # instead of stacks
+
+class StacksQueue():
+    def __init__(self):
+        self._main_stack = []
+
+    def enqueue(self, element):
+        self._main_stack.append(element)
+
+    def dequeue(self):
+        # Put everything from _main_stack to temp_stack
+        temp_stack = []
+
+        while(len(self._main_stack) > 0):
+            temp_stack.append(self._main_stack.pop())
+
+        element = temp_stack.pop()
+
+        while(len(temp_stack) > 0):
+            self._main_stack.append(temp_stack.pop())
+        
+        return element
+    
+    def size(self):
+        return len(self._main_stack)
+    
+    def is_empty(self):
+        return self.size() == 0
+
+if __name__=='__main__':
+    my_queue = StacksQueue()
+
+    for num in range(10):
+        my_queue.enqueue(num)
+
+    for _ in range(10):
+        print(my_queue.dequeue())
---
+++ queues/leet_code.py
@@ -0,0 +1,39 @@
+# Problem 1700(variant): number of Irie's friends unable to eat
+
+# We can simulate this, but do we have to? I think so, because:
+# ... can we come up with a counter example where (a) there are k
+# friends and (b) k sandwiches equal to the k friends, but there
+# are unfed friends?
+#
+# Attempt 1: [1 0 1 0] friends, [0 1 0 1] sandwiches
+# Attempt 2: [1 1] friends, [0 1] sandwiches ==> 2 friends go hungry
+# ... it seems like this can be computed without simulation, but
+#     let's simulate it for fun!
+import unittest
+
+def feeding_friends(friends, sandwiches):
+    passes_left = len(friends)
+
+    while(len(friends) > 0 and passes_left > 0):
+        friend = friends.pop(0)
+
+        if friend == sandwiches.get(-1):
+            sandwiches.pop()
+            passes_left = len(friends)
+        else:
+            friends.append(friend)
+            passes_left = passes_left - 1
+
+    return len(friends)
+
+# Friends       Sandwiches     Result
+# ===================================
+#  [1 1]         [0 0]           2
+#  [0 0]         [0 0]           0
+
+class FeedingFriendsTester(unittest.TestCase):
+    def test_all_eat(self):
+        self.assertFalse(True)
+
+if __name__=='__main__':
+    unittest.main()
